93517fb37ee01b0015d32b7030a0145d7cf6d17c
Klara Frommelin <freakyfrommelin@outlook.com>
2025-08-18 14:38:37 +0200

    Adding image generation in registraion

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './register.sass'
 import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
   const navigate = useNavigate()
+
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+    email: '',
+    avatar: ''
+  })
+
+  const updateUser = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value})
+  }
+
+  const randomizeAvatar = () => {
+    setUser({ ...user, avatar: `https://picsum.photos/200?random=${Math.floor(Math.random() * 1000)}`})
+  }
+
+  const isValidUrl = (link) => {
+    try {
+      return Boolean(new URL(link))
+    } catch {
+      return false
+    }
+  }
 
   const handleRegistration = (e) => {
     e.preventDefault()
@@ -14,46 +37,80 @@ const Register = () => {
   return (
     <form id='register' onSubmit={handleRegistration}> 
         <h1>Register account</h1>
-        <div className="input-group mb-3">
-            <span id='username' className="input-group-text">Username</span>
-            <input 
-              type="text" 
-              placeholder="Username" 
-              className="form-control" 
-              aria-label="Username" 
-              aria-describedby="username" 
-            />
+        <div id='formContent'>
+          <div>
+            <div className="input-group mb-3">
+              <span id='user' className="input-group-text">Username</span>
+              <input 
+                type="text" 
+                name='username'
+                placeholder="Username" 
+                value={user.username}
+                className="form-control" 
+                aria-label="Username" 
+                aria-describedby="user" 
+                onChange={updateUser}
+              />
+            </div>
+            <div className="input-group mb-3">
+                <span id='pwd' className="input-group-text">Password</span>
+                <input 
+                  type="password" 
+                  name='password'
+                  placeholder="Password" 
+                  value={user.password}
+                  className="form-control" 
+                  aria-label="Password" 
+                  aria-describedby="pwd" 
+                  onChange={updateUser}
+                />
+            </div>
+            <div className="input-group mb-3">
+                <span id='e-mail' className="input-group-text">E-mail</span>
+                <input 
+                  type="email" 
+                  name='email'
+                  placeholder="example@example.com"
+                  value={user.email} 
+                  className="form-control" 
+                  aria-label="Email" 
+                  aria-describedby="e-mail" 
+                  onChange={updateUser}
+                />
+            </div>
+            <div className="input-group mb-3">
+                <span id='profileimg' className="input-group-text">Avatar</span>
+                <input 
+                  type="hidden"
+                  name='avatar' 
+                  value={user.avatar}
+                  aria-label="Avatar" 
+                  aria-describedby="profileimg"
+                />
+                <button
+                  className='btn btn-outline-info'
+                  type='button'
+                  title='Randomize avatar'
+                  onClick={randomizeAvatar}
+                >
+                  Randomize image
+                </button>
+            </div>
+          </div>
+          
+          <div>
+            {(isValidUrl(user.avatar) && (
+              <img
+                src={user.avatar}
+                alt='Profile image preview'
+                className='img-thumbnail rounded'
+              />
+            )) || (!isValidUrl(user.avatar) && (
+              <p>Randomize image to see preview here</p>
+            ))}
+          </div>
         </div>
-        <div className="input-group mb-3">
-            <span id='password' className="input-group-text">Password</span>
-            <input 
-              type="password" 
-              placeholder="Password" 
-              className="form-control" 
-              aria-label="Password" 
-              aria-describedby="password" 
-            />
-        </div>
-        <div className="input-group mb-3">
-            <span id='email' className="input-group-text">E-mail</span>
-            <input 
-              type="email" 
-              placeholder="example@example.com" 
-              className="form-control" 
-              aria-label="Email" 
-              aria-describedby="email" 
-            />
-        </div>
-        <div className="input-group mb-3">
-            <span id='avatar' className="input-group-text">Avatar</span>
-            <input 
-              type="url" 
-              placeholder="Avatar URL" 
-              className="form-control" 
-              aria-label="Avatar" 
-              aria-describedby="avatar" 
-            />
-        </div>
+        
         <button type='submit' className='btn btn-outline-success'>Create account</button>
     </form>
   )
