5d949852156003401d979dc320cf8f1aa3462a0d
Klara Frommelin <freakyfrommelin@outlook.com>
2025-08-18 13:39:15 +0200

    Increasing readability in form inputs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,11 +18,23 @@ const Login = () => {
         <h1>Sign in</h1>
         <div className="input-group mb-3">
             <span id='username' className="input-group-text">Username</span>
-            <input type="text" placeholder="Username"  className="form-control" aria-label="Username" aria-describedby="username" />
+            <input 
+              type="text" 
+              placeholder="Username" 
+              className="form-control" 
+              aria-label="Username" 
+              aria-describedby="username" 
+            />
         </div>
         <div className="input-group mb-3">
             <span id='password' className="input-group-text">Password</span>
-            <input type="password" placeholder="Password"  className="form-control" aria-label="Password" aria-describedby="password" />
+            <input 
+              type="password" 
+              placeholder="Password" 
+              className="form-control" 
+              aria-label="Password" 
+              aria-describedby="password" 
+            />
         </div>
         <button type='submit' className='btn btn-outline-success'>Sign in</button>
     </form>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -16,19 +16,43 @@ const Register = () => {
         <h1>Register account</h1>
         <div className="input-group mb-3">
             <span id='username' className="input-group-text">Username</span>
-            <input type="text" placeholder="Username"  className="form-control" aria-label="Username" aria-describedby="username" />
+            <input 
+              type="text" 
+              placeholder="Username" 
+              className="form-control" 
+              aria-label="Username" 
+              aria-describedby="username" 
+            />
         </div>
         <div className="input-group mb-3">
             <span id='password' className="input-group-text">Password</span>
-            <input type="password" placeholder="Password"  className="form-control" aria-label="Password" aria-describedby="password" />
+            <input 
+              type="password" 
+              placeholder="Password" 
+              className="form-control" 
+              aria-label="Password" 
+              aria-describedby="password" 
+            />
         </div>
         <div className="input-group mb-3">
             <span id='email' className="input-group-text">E-mail</span>
-            <input type="email" placeholder="example@example.com"  className="form-control" aria-label="Email" aria-describedby="email" />
+            <input 
+              type="email" 
+              placeholder="example@example.com" 
+              className="form-control" 
+              aria-label="Email" 
+              aria-describedby="email" 
+            />
         </div>
         <div className="input-group mb-3">
             <span id='avatar' className="input-group-text">Avatar</span>
-            <input type="url" placeholder="Avatar URL"  className="form-control" aria-label="Avatar" aria-describedby="avatar" />
+            <input 
+              type="url" 
+              placeholder="Avatar URL" 
+              className="form-control" 
+              aria-label="Avatar" 
+              aria-describedby="avatar" 
+            />
         </div>
         <button type='submit' className='btn btn-outline-success'>Create account</button>
     </form>
